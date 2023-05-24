d011c465a4789ad61fb55d6c1ce20b8e454e7c1c
fanura <81693732+fanura@users.noreply.github.com>
2023-05-24 23:42:15 +0700

    revisi crosy road

---
+++ Assets/KillPlayerOnTouch.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillPlayerOnTouch : MonoBehaviour
+{
+        private void OnCollisionEnter(Collision collision)
+        {
+            if(collision.collider.GetComponent<Player>() != null)
+            {
+                Destroy(collision.gameObject);
+            }
+        }
+
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private TerrainGenerator terrainGenerator;
+    [SerializeField] private Text scoreText;
+    private int score;
+    private Animator animator;
+    private bool isHopping;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+    /*private void FixedUpdate()
+       {
+            score++;
+       }*/
+   private void Update()
+   {
+        
+        //scoreText.text = "Score : " + score;
+        if (Input.GetKeyDown(KeyCode.W) && !isHopping)
+        {
+
+            float zDifference = 0;
+            if (transform.position.z % 1 != 0)
+            {
+                zDifference = Mathf.Round(transform.position.z) - transform.position.z;
+            }
+            MoveCharacter(new Vector3(1, 0, zDifference));
+        }
+        else if (Input.GetKeyDown(KeyCode.A) && !isHopping)
+        {
+
+            MoveCharacter(new Vector3(0, 0, 1));
+        }
+        else if (Input.GetKeyDown(KeyCode.D) && !isHopping)
+        {
+
+            MoveCharacter(new Vector3(0, 0, -1));
+        }
+   }
+
+   private void MoveCharacter(Vector3 difference)
+   {
+            animator.SetTrigger("hop");
+            isHopping = true;
+            transform.position = (transform.position + difference);
+
+            terrainGenerator.SpawnTerrain(false, transform.position);
+   }
+
+   public void FinishHop()
+   {
+        isHopping = false;
+   }
+}
